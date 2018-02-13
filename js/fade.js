7d2e0ec5a2fd2cda8c58505d9bdc0dfc8275bd81
solstacedesigns <36145111+solstacedesigns@users.noreply.github.com>
2018-02-12 23:31:30 -0500

    Add files via upload

---
+++ js/fade.js
@@ -0,0 +1,29 @@
+var animateHTML = function () {
+  var elems,
+    windowHeight
+  var init = function () {
+    elems = document.getElementsByClassName('hidden')
+    windowHeight = window.innerHeight
+    _addEventHandlers()
+  }
+  var _addEventHandlers = function () {
+    window.addEventListener('scroll', _checkPosition)
+    window.addEventListener('resize', init)
+  }
+  var _checkPosition = function () {
+    for (var i = 0; i < elems.length; i++) {
+      var posFromTop = elems[i].getBoundingClientRect().bottom - 100;
+      if (posFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace('hidden', 'fade-in-element')
+      }
+    }
+  }
+  return {
+    init: init
+  }
+}
+animateHTML().init()
+window.onload = function() {
+window.scrollTo(0, 1);
+window.scrollTo(0, 0);
+};
